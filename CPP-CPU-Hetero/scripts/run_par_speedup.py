95568e4e2b4e496d2afaecac519342864241937a
asenjo <asenjo@uma.es>
2023-03-14 19:20:03 +0100

    Speedup results for the best level-minradius configuration

---
+++ CPP-CPU-Hetero/scripts/run_par_speedup.py
@@ -0,0 +1,48 @@
+import os
+import time
+import numpy as np
+
+#Sliding window size
+Wsize = 10
+#Gris size for stage 3 (filling empty areas of Bsize*Bsize with a minimum point)
+Bsize = 20
+#Overlap for the sliding window. Displacement will be Wsize(1-Overlap)=10m*0.2=2m
+Overlap = 0.8
+#num_threads for the openmp implementations of stage1 and stage3
+num_threads = [1, 2, 4, 6, 8]
+#number of times the OWM is executed
+nreps = 5
+maxNumber=32
+
+output="o4_speedup.out"
+
+start = time.time()
+print("Start : %s" % time.ctime())
+f = open(output, "a")
+f.write("Start : {}".format(time.ctime()))
+f.close()
+
+executable_par="../bin/o3memo"
+inputs=["../bin/data/AlcoyH",
+        "../bin/data/ArzuaH",
+        "../bin/data/BrionFH",
+        "../bin/data/BrionUH"]
+
+minRadius=[1.8,0.5,0.1,0.1] #best MR for each cloud and o4
+levels = [5,5,4,4] #best level for each cloud and o4
+
+for file,mR,lev in zip(inputs,minRadius,levels):
+    for nth in num_threads:
+        print("Running: {} -i {} -W {} -B {} -O {} -n {} -l {} -r {} -s {} -L {}".format(executable_par,file,Wsize,Bsize,Overlap,nth,nreps,mR,maxNumber,lev))
+        f = open(output, "a")
+        f.write("\n\nRunning: {} -i {} -W {} -B {} -O {} -n {} -l {} -r {} -s {} -L {}\n\n".format(executable_par,file,Wsize,Bsize,Overlap,nth,nreps,mR,maxNumber,lev))
+        f.close()
+        os.system("%s -i %s -W %d -B %d -O %f -n %d -l %d -r %f -s %d -L %d| tee -a %s" % (executable_par, file, Wsize, Bsize, Overlap, nth, nreps,mR, maxNumber, lev, output))
+
+end = time.time()
+print("End : %s" % time.ctime())
+print("Total Execution time: %f hours" % ((end - start)/3600))
+f = open(output, "a")
+f.write("End : {}".format(time.ctime()))
+f.write("Total Execution time: {} hours".format((end - start)/3600))
+f.close()
