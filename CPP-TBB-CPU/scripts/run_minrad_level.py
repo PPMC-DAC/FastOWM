60514aa22e08ffac731a9dff3b0b7f9aa8cc4278
FelipeMLopez <12429461+FelipeMLopez@users.noreply.github.com>
2024-02-23 12:14:07 +0100

    update scripts o3 and o4_minRad

--- CPP-TBB-CPU/scripts/run_minrad_level.py
+++ CPP-TBB-CPU/scripts/run_minrad_level.py
@@ -1,6 +1,10 @@
 import os
 import time
 import numpy as np
+from datetime import datetime
+
+# get the number of physical cores
+nprocs = int(os.popen("lscpu | grep 'Core(s) per socket' | awk '{print $4}'").read().strip())
 
 #Sliding window size
 Wsize = 10
@@ -9,42 +13,51 @@ Bsize = 20
 #Overlap for the sliding window. Displacement will be Wsize(1-Overlap)=10m*0.2=2m
 Overlap = 0.8
 #num_threads for the openmp implementations of stage1 and stage3
-num_threads = [1, 2, 4, 6, 8]
+num_threads = np.insert(np.linspace(2, nprocs, nprocs//2, dtype=int, endpoint=True), 0, 1)
 #number of times the OWM is executed
 nreps = 5
 maxNumber=65536
 
-output="minrad_level.out"
-
-start = time.time()
-print("Start : %s" % time.ctime())
-f = open(output, "a")
-f.write("Start : {}".format(time.ctime()))
-f.close()
-
 executable_par="../bin/o3memo"
-inputs=["../bin/data/AlcoyH",
-        "../bin/data/ArzuaH",
-        "../bin/data/BrionFH",
-        "../bin/data/BrionUH"]
+inputs=[
+    "../bin/data/AlcoyH",
+    "../bin/data/ArzuaH",
+    "../bin/data/BrionFH",
+    "../bin/data/BrionUH",
+    ]
+
+# get the hostname
+hostname = os.popen("hostname").read().strip()
+# set the output file
+output = f'o4_minradius_{hostname}.out'
 
 minRadius=[x/10 for x in list(range(1,20,1))]
 levels = list(range(3,10))
 nth=8
 
-for file in inputs:
-    for mR in minRadius:
-        for lev in levels:
-            print("Running: {} -i {} -W {} -B {} -O {} -n {} -l {} -r {} -s {} -L {}".format(executable_par,file,Wsize,Bsize,Overlap,nth,nreps,mR,maxNumber,lev))
-            f = open(output, "a")
-            f.write("\n\nRunning: {} -i {} -W {} -B {} -O {} -n {} -l {} -r {} -s {} -L {}\n\n".format(executable_par,file,Wsize,Bsize,Overlap,nth,nreps,mR,maxNumber,lev))
-            f.close()
-            os.system("%s -i %s -W %d -B %d -O %f -n %d -l %d -r %f -s %d -L %d| tee -a %s" % (executable_par, file, Wsize, Bsize, Overlap, nth, nreps,mR, maxNumber, lev, output))
+start = time.time()
+print("Start : %s" % time.ctime())
 
-end = time.time()
+with open(output, "a") as f:
+    # stamp the start time
+    f.write(f'Start: {datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}')
+    # iterate over the clouds, levels and number of threads
+    for cloud in inputs:
+        for mR in minRadius:
+            for lev in levels:
+                print("Running: {} -i {} -W {} -B {} -O {} -n {} -l {} -r {} -s {} -L {}".format(executable_par,cloud,Wsize,Bsize,Overlap,nth,nreps,mR,maxNumber,lev))
+                # save the configuration in the file
+                f.write("\n\nRunning: {} -i {} -W {} -B {} -O {} -n {} -l {} -r {} -s {} -L {}\n\n".format(executable_par,cloud,Wsize,Bsize,Overlap,nth,nreps,mR,maxNumber,lev))
+                # flush the buffer
+                f.flush()
+                # excecute the command and save the output to the file
+                os.system("%s -i %s -W %d -B %d -O %f -n %d -l %d -r %f -s %d -L %d| tee -a %s" % (executable_par, cloud, Wsize, Bsize, Overlap, nth, nreps,mR, maxNumber, lev, output))
+
+    end = time.time()
+    f.write(f'End: {datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}\n')
+    f.write("Total Execution time: {} hours\n".format((end - start)/3600))
+
 print("End : %s" % time.ctime())
 print("Total Execution time: %f hours" % ((end - start)/3600))
-f = open(output, "a")
-f.write("End : {}".format(time.ctime()))
-f.write("Total Execution time: {} hours".format((end - start)/3600))
-f.close()
+# copy the output file to the results folder
+os.system(f"cp {output} ../../Results/{output.replace('.out', '.txt')}")
--- CPP-TBB-CPU/scripts/run_par_optim3.py
+++ CPP-TBB-CPU/scripts/run_par_optim3.py
@@ -17,42 +17,49 @@ num_threads = np.insert(np.linspace(2, nprocs, nprocs//2, dtype=int, endpoint=Tr
 #number of times the OWM is executed
 nreps = 5
 
-start = time.time()
-print("Start : %s" % time.ctime())
-
-executable_par="../bin/o3memo"
-inputs=["../bin/data/AlcoyH",
-        "../bin/data/ArzuaH",
-        "../bin/data/BrionFH",
-        "../bin/data/BrionUH"]
+executable_list=['../bin/o3memoA', '../bin/o3memo', '../bin/o3memoB']
+inputs=[
+    "../bin/data/AlcoyH",
+    "../bin/data/ArzuaH",
+    "../bin/data/BrionFH",
+    "../bin/data/BrionUH",
+    ]
 
 # get the hostname
 hostname = os.popen("hostname").read().strip()
 # set the output file
-output = f'o3_memoization_{hostname}.out'
+output_list = [f'o3_memoizationA_{hostname}.out', f'o3_memoization_{hostname}.out', f'o3_memoizationB_{hostname}.out']
 
 levels = list(range(2,10))
 mR=0.1
 maxNumber=32
 
-with open(output, "a") as f:
-    f.write(f'Start: {datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}')
-    for cloud in inputs:
-        for lev in levels:
-            for nth in num_threads:
-                print("Running: {} -i {} -W {} -B {} -O {} -n {} -l {} -r {} -s {} -L {}".format(executable_par,cloud,Wsize,Bsize,Overlap,nth,nreps,mR,maxNumber,lev))
-                # save the configuration in the file
-                f.write("\n\nRunning: {} -i {} -W {} -B {} -O {} -n {} -l {} -r {} -s {} -L {}\n\n".format(executable_par,cloud,Wsize,Bsize,Overlap,nth,nreps,mR,maxNumber,lev))
-                # flush the buffer
-                f.flush()
-                # excecute the command and save the output to the file
-                os.system("%s -i %s -W %d -B %d -O %f -n %d -l %d -r %f -s %d -L %d| tee -a %s" % (executable_par, cloud, Wsize, Bsize, Overlap, nth, nreps,mR, maxNumber, lev, output))
+# zip the output and executable lists
+for output, executable_par in zip(output_list, executable_list):
 
-    end = time.time()
-    f.write(f'End: {datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}')
-    f.write("Total Execution time: {} hours".format((end - start)/3600))
+    start = time.time()
+    print("Start : %s" % time.ctime())
 
-print("End : %s" % time.ctime())
-print("Total Execution time: %f hours" % ((end - start)/3600))
-# copy the output file to the results folder
-os.system(f"cp {output} ../../Results/{output.replace('.out', '.txt')}")
+    with open(output, "a") as f:
+        # stamp the start time
+        f.write(f'Start: {datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}')
+        # iterate over the clouds, levels and number of threads
+        for cloud in inputs:
+            for lev in levels:
+                for nth in num_threads:
+                    print("Running: {} -i {} -W {} -B {} -O {} -n {} -l {} -r {} -s {} -L {}".format(executable_par,cloud,Wsize,Bsize,Overlap,nth,nreps,mR,maxNumber,lev))
+                    # save the configuration in the file
+                    f.write("\n\nRunning: {} -i {} -W {} -B {} -O {} -n {} -l {} -r {} -s {} -L {}\n\n".format(executable_par,cloud,Wsize,Bsize,Overlap,nth,nreps,mR,maxNumber,lev))
+                    # flush the buffer
+                    f.flush()
+                    # excecute the command and save the output to the file
+                    os.system("%s -i %s -W %d -B %d -O %f -n %d -l %d -r %f -s %d -L %d| tee -a %s" % (executable_par, cloud, Wsize, Bsize, Overlap, nth, nreps,mR, maxNumber, lev, output))
+
+        end = time.time()
+        f.write(f'End: {datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}\n')
+        f.write("Total Execution time: {} hours\n".format((end - start)/3600))
+
+    print("End : %s" % time.ctime())
+    print("Total Execution time: %f hours" % ((end - start)/3600))
+    # copy the output file to the results folder
+    os.system(f"cp {output} ../../Results/{output.replace('.out', '.txt')}")
