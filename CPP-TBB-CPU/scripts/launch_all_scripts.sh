873b739454517f73ab534f9c4985f9afbdc341c7
Felipe Munoz <12429461+FelipeMLopez@users.noreply.github.com>
2024-02-26 20:22:35 +0100

    Add run_seq to avoid in launch script

--- CPP-TBB-CPU/scripts/launch_all_scripts.sh
+++ CPP-TBB-CPU/scripts/launch_all_scripts.sh
@@ -3,7 +3,8 @@
 # run through all the scripts in the directory with "run_" in the name
 for script in run_*.py; do
     # Skip specific scripts
-    if [[ "$script" == "run_par_baseline.py" || "$script" == "run_par_rev1.py" ]]; then
+    # if [[ "$script" == "run_seq.py" || "$script" == "run_par_rev1.py" ]]; then
+    if ["$script" == "run_seq.py"]; then
         echo "Skipping $script"
     else
         echo "Running $script"
