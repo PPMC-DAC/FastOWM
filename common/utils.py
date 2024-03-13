97a43869ecd78373950d91244f87992ded9de58f
FelipeMLopez <12429461+FelipeMLopez@users.noreply.github.com>
2024-03-13 17:47:41 +0100

    Update functions in Utils

--- common/utils.py
+++ common/utils.py
@@ -20,6 +20,25 @@ def get_nprocs():
         # list of number of threads to use: 1, 2, ... nprocs, with step 2
         return np.insert(np.arange(2, nprocs+1, 2), 0, 1)
     
-def get_best(results, cloud, nth, key='total'):
+def get_best_level(results, cloud, nth, key='total'):
+    """
+        Get the best level for the given parameters
+    """
     # get the best level or chunk for the given parameters
     return min(results[cloud], key=lambda x: results[cloud][x][nth][key])
+
+def get_best_level2(ires, nth, pos=2):
+    """
+        Get the best level for the given parameters
+    """
+    # get the best level or chunk for the given parameters
+    return min(ires, key=lambda x: ires[x][nth][pos])
+
+def get_best_comb(ires, num_threads, pos=2):
+    """
+        Get the best level, num processes and time for the given parameters
+    """
+    # create a flattened list of all combinations of x and y along with the time
+    flattened = [(x, y, ires[x][y][pos]) for x in ires for y in num_threads]
+    # find the minimum based on Total time; this return bestlevel, bestthreads, time
+    return min(flattened, key=lambda item: item[2])
