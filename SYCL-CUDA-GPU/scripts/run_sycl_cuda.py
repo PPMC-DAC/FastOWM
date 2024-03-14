40c0b8c3fe45087d802c938b2de9e9216b00389c
FelipeMLopez <12429461+FelipeMLopez@users.noreply.github.com>
2024-03-14 19:18:14 +0100

    Select different number of threads in SYCL-cpu script

--- SYCL-CUDA-GPU/scripts/run_sycl_cuda.py
+++ SYCL-CUDA-GPU/scripts/run_sycl_cuda.py
@@ -1,6 +1,8 @@
 import os
 import time
 from datetime import datetime
+from common.utils import get_nprocs
+
 
 # name of the input files without the extension .xyz
 inputs=[
@@ -14,21 +16,32 @@ inputs=[
 hostname = os.popen("hostname").read().strip()
 # set the output file
 output = f"sycl_cuda_{hostname}.out"
+#num_threads for the openmp implementations of stage1 and stage3
+num_threads = get_nprocs()
 
 # executables
 executable_list = [ 
                     "../bin/owm-sycl-cpu",
                     "../bin/owm-sycl-cpu-nomemo",
-                    "../bin/owm-sycl-igpu",
-                    "../bin/owm-sycl-igpu-nomemo",
-                    "../bin/owm-sycl-dgpu",
-                    "../bin/owm-sycl-dgpu-nomemo",
-                    "../bin/owm-cuda",
-                    "../bin/owm-cuda-grid",
-                    "../bin/owm-cuda-nomemo",
+                    # "../bin/owm-sycl-igpu",
+                    # "../bin/owm-sycl-igpu-nomemo",
+                    # "../bin/owm-sycl-dgpu",
+                    # "../bin/owm-sycl-dgpu-nomemo",
+                    # "../bin/owm-cuda",
+                    # "../bin/owm-cuda-grid",
+                    # "../bin/owm-cuda-nomemo",
                 ]
 
 maxNumber=[4,8,16,32,64,128,256,512,1024]
+
+# select the number of threads
+if hostname == 'alder':
+    # only p-cores, and p-cores + e-cores
+    vnth = [8,16]
+elif hostname == 'bombay':
+    vnth = [32,48,64,80,96]
+else:
+    vnth = [num_threads[-1]]
 
 start = time.time()
 print("Start : %s" % time.ctime())
@@ -40,15 +53,28 @@ with open(output, "a") as f:
     for exe in executable_list:
         for i in inputs:
             for mN in maxNumber:
-                print("\n***************\nRunning: {} {} {}".format(exe, i, mN))
-                # save the configuration in the file
-                f.write("\n\nRunning: {} {} {}\n\n".format(exe, i, mN))
-                # flush the buffer
-                f.flush()
-                # execute the command and save the output to the file
-                os.system("%s %s %d | tee -a %s" % (exe, i, mN, output))
-                # sleep until the next execution
-                time.sleep(30)
+                if 'cpu' in exe:
+                    # if cpu version, iterate over the number of threads
+                    for nth in vnth:
+                        print("\n***************\nRunning: {} {} {} {}".format(exe, i, mN, nth))
+                        # save the configuration in the file
+                        f.write("\n\nRunning: {} {} {} {}\n\n".format(exe, i, mN, nth))
+                        # flush the buffer
+                        f.flush()
+                        # execute the command and save the output to the file
+                        os.system("DPCPP_CPU_NUM_CUS=%d %s %s %d | tee -a %s" % (nth, exe, i, mN, output))
+                        # sleep until the next execution
+                        time.sleep(30)
+                else:
+                    print("\n***************\nRunning: {} {} {}".format(exe, i, mN))
+                    # save the configuration in the file
+                    f.write("\n\nRunning: {} {} {}\n\n".format(exe, i, mN))
+                    # flush the buffer
+                    f.flush()
+                    # execute the command and save the output to the file
+                    os.system("%s %s %d | tee -a %s" % (exe, i, mN, output))
+                    # sleep until the next execution
+                    time.sleep(30)
 
     end = time.time()
     f.write(f'End: {datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}\n')
